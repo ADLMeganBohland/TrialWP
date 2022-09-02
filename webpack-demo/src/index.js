56378acf51123907e34c2b92b5c01caf7bab7b9f
Megan Bohland <megan.bohland.ctr@adlnet.gov>
2022-09-02 10:15:36 -0400

    Working through tutorial two

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './smile.png';
 
 function component() {
-    const element = document.createElement('div');
+    const element = document.createElement('div'); //AHH this makes the DIV!!!
   
+    //Now they are adding stuff to the div!
+
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
+
+      // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
     return element;
   }
-  
+  //appending div to body?
   document.body.appendChild(component());
