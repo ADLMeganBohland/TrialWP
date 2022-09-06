7a7cd098ba917b71bad1d55b60871967d985050b
Megan Bohland <megan.bohland.ctr@adlnet.gov>
2022-09-06 12:57:35 -0400

    This is fourth tutorial with debug, delib error

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,20 @@
 import _ from 'lodash';
-
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div'); //AHH this makes the DIV!!!
-  
+    const btn = document.createElement('button'); //This is making a button!!
+    
+    //is inner html text? see button below
     //Now they are adding stuff to the div!
-
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    //the html for the button, so the text? 
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    //appened the button to the div? 
+    element.appendChild(btn);
   
     return element;
   }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,15 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-  mode:
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     }),
   ],
    output: {
